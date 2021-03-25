44035b5b0eb75991cb98c19ad0c947330f50a7fe
x14102004x <marcosproenca144@gmail.com>
2021-03-25 01:03:33 -0300

    Added: Babel & Webpack

---
+++ web/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import aprovado from "./assets/aprovado.svg";
+
+function App() {
+  return (
+    <>
+      <img src={aprovado} alt="aprovado" />
+    </>
+  );
+}
+
+export default App;
