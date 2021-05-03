a3e19f7cd12ed7a12476ca7e75b9af70eb5db55a
x14102004x <marcosproenca144@gmail.com>
2021-05-02 22:53:14 -0300

    feat: use persisted state hook was added

---
+++ src/hooks/usePersistedState.ts
@@ -0,0 +1,19 @@
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+
+type IResponse<T> = [T, Dispatch<SetStateAction<T>>];
+
+function usePersistedState<T>(key: string, initialState: T): IResponse<T> {
+  const [state, setState] = useState(() => {
+    const storage = localStorage.getItem(key);
+
+    if (storage) return JSON.parse(storage);
+    return initialState;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+  return [state, setState];
+}
+
+export default usePersistedState;
